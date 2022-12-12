f0e4d3267848df383bc2ccdcc6b67ef643886447
Freddie-Faulkner03 <77018972+Freddie-Faulkner03@users.noreply.github.com>
2022-12-12 22:27:21 +0000

    added comments to youtube tutorial file

--- app/src/main/java/uk/co/freddiefaulkner/supercars/youtubeTutorial.java
+++ app/src/main/java/uk/co/freddiefaulkner/supercars/youtubeTutorial.java
@@ -2,12 +2,14 @@ package uk.co.freddiefaulkner.supercars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+// imports stuff from android library
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+// imports stuff from google/youtube library
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -15,25 +17,34 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 
 public class youtubeTutorial extends YouTubeBaseActivity {
+    // creates a new youtube player view for my video
     YouTubePlayerView youTubePlayerView;
+    // creates a listener which will be used to check for video
     YouTubePlayer.OnInitializedListener onInitializedListener;
+    // button created to allow user to play video
     Button btnPlay;
+    // this bool is used to allow me to mute/unmute the sound
     boolean soundOn;
+    // media player used to allow video to play in backend
     MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // loads the screen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube_tutorial);
 
+        // starts and sets up the youtube video
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.j);
         mediaPlayer.start();
         mediaPlayer.setLooping(true);
 
+        // sets up button, player and textbox so it can perform tasks
         btnPlay = findViewById(R.id.btnPlay);
         youTubePlayerView = findViewById(R.id.youtube_player);
         final TextView txtFail = findViewById(R.id.txtFail);
 
+        // sets up video to either load is connected to internet
         onInitializedListener = new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
@@ -41,12 +52,14 @@ public class youtubeTutorial extends YouTubeBaseActivity {
                 youTubePlayer.loadVideo("6RNtB--oGiw");
             }
 
+            // shows error if video fails to load
             @Override
             public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
                 txtFail.setText("Video Failed To Load!");
             }
         };
 
+        // plays the video
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +69,7 @@ public class youtubeTutorial extends YouTubeBaseActivity {
 
     }
 
+    // function used to check if sound is on or off if mute is pressed
     public void sound(View view) {
         if(!soundOn){
             mediaPlayer.start();
@@ -67,6 +81,7 @@ public class youtubeTutorial extends YouTubeBaseActivity {
         }
     }
 
+    // when navigating away form is destroyed
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -74,6 +89,7 @@ public class youtubeTutorial extends YouTubeBaseActivity {
         mediaPlayer.release();
     }
 
+    // sets up form if navigated back onto the form
     @Override
     protected void onRestart() {
         super.onRestart();
