1e74d27ad33b87649cffda7edb3fdf3362827e49
Freddie-Faulkner03 <77018972+Freddie-Faulkner03@users.noreply.github.com>
2022-12-12 22:29:47 +0000

    added comments to youtube config file

--- app/src/main/java/uk/co/freddiefaulkner/supercars/YoutubePlayerConfig.java
+++ app/src/main/java/uk/co/freddiefaulkner/supercars/YoutubePlayerConfig.java
@@ -2,12 +2,15 @@ package uk.co.freddiefaulkner.supercars;
 
 public class YoutubePlayerConfig {
 
+    // empty constructor used to create instance of this class
     public YoutubePlayerConfig(){
 
     }
 
+    // declares the constant string which is the api key needed for my youtube video
     private static final String API_KEY = "AIzaSyCgD8ZQebCAT3rgGGAeXzxiGaUcUqWRz6A";
 
+    // function used to return the api key
     public static String getApiKey(){
         return API_KEY;
     }
